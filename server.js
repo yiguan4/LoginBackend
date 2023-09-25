0ac7ce8f9338bdd09019202024bbc02ee34bd396
yiguan4 <0420viviane@gmail.com>
2023-09-24 18:12:04 -0700

    Created collection:accounts at MongoDB

--- server.js
+++ server.js
@@ -3,13 +3,50 @@ const keys = require('./config/keys.js');
 
 const app = express();
 
+// DB
 const moogoose = require('mongoose');
 moogoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
+// DB models
+require('./model/Account');
+const Account = moogoose.model('accounts');
+
 // Routes
-app.get('/auth', async (req, res) => {
-    console.log(req.query);
-    res.send("hello" + req.query.userId + "this is " + Date.now());
+app.get('/account', async (req, res) => {
+    const { cUsername, cPassword } = req.query;
+
+    if(cUsername == null || cPassword == null){
+        res.send("Invalid credentials");
+        return;
+    }
+
+    var userAccount = await Account.findOne({ username : cUsername});
+    if(userAccount == null){
+        console.log("Create new account");
+
+        var newAccount = new Account({
+            username : cUsername,
+            password : cPassword,
+
+            lastAuthentication : Date.now()
+        });
+
+        await newAccount.save();
+
+        res.send(newAccount);
+        return;
+    }else{
+        if(cPassword == userAccount.password){
+            userAccount.lastAuthentication = Date.now();
+            await userAccount.save();
+            
+            console.log("retrieving ")
+            res.send(userAccount);
+            return;
+        }
+    }
+
+    res.send('hello');
 });
 
 const port = 1234;
