bbb5c21b35ca48e171e1e6b99ea8edb0d2cae340
yiguan4 <0420viviane@gmail.com>
2023-09-26 12:38:49 -0700

    Added get for testing purpose

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -2,6 +2,31 @@ const moogoose = require('mongoose');
 const Account = moogoose.model('accounts');
 
 module.exports = app => {
+    
+    //Testing purpose
+    app.get('/auth', async(req, res) => {
+        const {cUsername, cPassword} = req.query;
+        if(cUsername == null || cPassword == null){
+            res.send("Invalid credentials");
+            return;
+        }
+        var userAccount = await Account.findOne({ username : cUsername});
+        if(userAccount != null){
+            if(cPassword == userAccount.password){
+                userAccount.lastAuthentication = Date.now();
+                await userAccount.save();
+                
+                console.log("Retrieving account ")
+                res.send(userAccount);
+                return;
+            }
+        }
+
+        res.send("Invalid credentials");
+        return;
+
+
+    });
 
     // Routes
     app.post('/account/login', async (req, res) => {
